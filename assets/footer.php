c5900da2bc3a0d3c6798a5602c823363b0241761
chrischivlog <chrischivlog@gmail.com>
2020-08-20 13:28:54 +0200

    added support anfrage button

--- assets/footer.php
+++ assets/footer.php
@@ -1,11 +1,18 @@
 
-
 <div class="footer">
 
     <div class="contentBox1">
 <center>
+
+<?php if(empty($_SESSION['admin'])) {?>
+<h3>Dein Anliegen ist nicht aufgeführt?</h3>
+<div class="tag-start">Support Anfrage stellen</div>
+<br>
+<?php }?>
+<br><br>
     <a href="<?php echo $impressum; ?>" style="color:white;">Impressum </a> |
-        <a href="<?php echo $datenschutz; ?>" style="color:white;">Datenschutz </a>
+        <a href="<?php echo $datenschutz; ?>" style="color:white;">Datenschutz </a>|
+         <a href="<?php echo $agb; ?>" style="color:white;">Inhalt</a>
 
         <?php if(empty($agb)){} else{ ?>
             | <a href="<?php echo $agb; ?>" style="color:white;">AGB </a>
