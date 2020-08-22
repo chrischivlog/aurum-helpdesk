713a1ccc4df3d7189c50d4ce90b640eb954bd8b8
chrischivlog <chrischivlog@gmail.com>
2020-08-22 11:23:57 +0200

    Update check_if_empty.php

--- sites/mail_system/class/check_if_empty.php
+++ sites/mail_system/class/check_if_empty.php
@@ -6,7 +6,7 @@ if($_GET['type'] == 'apply') {
         $cat_check = $_GET['cat']; ///DEFINE CAT TO REDIRECT
         if(empty($_POST['user_name'] && $_POST['mail'] && $_POST['request1'] && $_POST['request2'] && $_POST['request3'] )){
             ///SEND BACK TO FORM
-            header("Refresh:4; url=../bugreport/");
+            header("Refresh:4; url=../join/formular.php?cat=$cat_check");
             echo"<h1>Bitte verwende eine Browser der <kbd>required</kbd> als HTML Attribute zulässt!</h1>";
             exit();
         } else {
@@ -17,9 +17,9 @@ if($_GET['type'] == 'apply') {
 
 //CHECK IF FORMS EMPTY SUPPORT NOT WITH REQUEST 1 CAUSE not empty by default
 if($_GET['type'] == 'support') {
-    if(empty($_POST['user_name'] && $_POST['mail'] && $_POST['request2'] && $_POST['request3'])){
+    if(empty($_POST['mail'] && $_POST['request2'] && $_POST['request3'])){
         ///SEND BACK TO FORM
-        header("Refresh:4; url=../bugreport/");
+        header("Refresh:4; url=../support-request/");
         echo"<h1>Bitte verwende eine Browser der <kbd>required</kbd> als HTML Attribute zulässt!</h1>";
         exit();
     } else {
@@ -44,7 +44,7 @@ if($_GET['type'] == 'bug') {
 if($_GET['type'] == 'bann') {
         if(empty($_POST['user_name'] && $_POST['mail'] && $_POST['request1'] && $_POST['request3'] && $_POST['request4'])){
             ///SEND BACK TO FORM
-            header("Refresh:4; url=../bugreport/");
+            header("Refresh:4; url=../unban/");
             echo"<h1>Bitte verwende eine Browser der <kbd>required</kbd> als HTML Attribute zulässt!</h1>";
             exit();
         } else {
